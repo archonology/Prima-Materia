9ae0703ae5e66e84bee6f6d6658596818c4c9ff9
Reed Meher <reed@meherdevs.com>
2024-06-07 13:46:38 -0500

    manifest json change

--- client/api/config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-//needs mongodb location
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/primaDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
--- client/api/models/index.js
+++
@@ -1,4 +0,0 @@
-const User = require("./User");
-const Deck = require("./Deck");
-
-module.exports = { User, Deck };
